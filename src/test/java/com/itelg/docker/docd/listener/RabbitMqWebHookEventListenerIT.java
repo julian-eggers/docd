5a29bd5d533d930ff0db657c29b73040648e4b02
jeggers <jeggers@IT-NB-02>
2020-03-14 00:01:39 +0100

    release

--- src/test/java/com/itelg/docker/docd/listener/RabbitMqWebHookEventListenerIT.java
+++ src/test/java/com/itelg/docker/docd/listener/RabbitMqWebHookEventListenerIT.java
@@ -2,7 +2,7 @@ package com.itelg.docker.docd.listener;
 
 import com.itelg.docker.docd.test.support.AbstractIT;
 
-public class AwsSqsWebHookEventListenerIT extends AbstractIT
+public class RabbitMqWebHookEventListenerIT extends AbstractIT
 {
     // TODO add tests
 }
